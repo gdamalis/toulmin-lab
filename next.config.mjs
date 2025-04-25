316ccd385e31c92d901f88842f9decf6338c8e18
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-04-25 01:21:39 -0300

    chore: update Next.js configuration to support .mjs file handling and remove swcMinify

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['firebasestorage.googleapis.com'], // In case we want to show images from Firebase Storage
+  },
+  turbopack: {
+    rules: {
+      '*.mjs': {
+        loaders: [], // Empty array, as we're just changing the type
+        as: '*.js', // This indicates to treat .mjs files as JavaScript
+      }
+    }
   },
   webpack: (config) => {
     // This is to make React Flow work with Next.js
