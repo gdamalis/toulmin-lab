b73fefdf6af56206dbb16b4cffb221cd550f77c0
Gabriel Damalis <gabrieldamalis@gmail.com>
2025-12-24 19:23:02 -0300

    chore: simplify ESLint configuration by removing unused imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,5 @@
 import nextConfig from "eslint-config-next";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
 
-const eslintConfig = [
-  ...nextConfig,
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-];
+const eslintConfig = [...nextConfig];
 
 export default eslintConfig;
